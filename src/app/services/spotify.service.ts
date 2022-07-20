6720e8813eb8fcbe2eef3003265163d58e0a3571
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-07-20 16:00:27 -0500

    spotiapp | Aplicando operador map de rxjs

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,11 @@ export class SpotifyService {
       'Authorization':'Bearer BQD3L3bGY-9RNAccVHlfIyueCNQio0RnWYinGZlp88Uvu73XjuNpG95fcO9nKPyIh1aOQbm-CiY_Whvk9o0dgT8FmE6b8TfsL2CjZP5pUbACv9M8TQE'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+      .pipe( map( (data:any) => {
+      return data.albums.items;
+      }) 
+    )
   }
 
   getArtista( termino: string ){
@@ -24,6 +29,9 @@ export class SpotifyService {
       'Authorization':'Bearer BQD3L3bGY-9RNAccVHlfIyueCNQio0RnWYinGZlp88Uvu73XjuNpG95fcO9nKPyIh1aOQbm-CiY_Whvk9o0dgT8FmE6b8TfsL2CjZP5pUbACv9M8TQE'
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+      .pipe(
+        map( ( data:any ) => data.artists.items )
+      );
   }
 }
