6ca8647d31ab040a23408caa2dfec95546b52b67
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-07-20 13:24:28 -0500

    spotiapp | Solicitando informacion de spotify new releases

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify Service listo')
+  }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQBGMvto-AThINEuquS4Fy6z3IE3tEufaCi5fe4zoJrd-pHAOU7kYE5vvnglpXHKZ8aeyFrDUyNsX2uCHnyFg2bgttFn_ldb-NyRNllvv1KLvksWYHY'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers }).subscribe( data => {
+      console.log(data)
+    });
+  }
+}
