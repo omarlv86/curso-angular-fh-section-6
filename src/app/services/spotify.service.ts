c0bd1aaf55cbd2fc6afd83c9a86ec3632322fd85
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-07-20 13:41:40 -0500

    spotiapp | Consumiendo informacion de new releases y mostrando cards en homeComponent

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,8 +16,6 @@ export class SpotifyService {
       'Authorization':'Bearer BQBGMvto-AThINEuquS4Fy6z3IE3tEufaCi5fe4zoJrd-pHAOU7kYE5vvnglpXHKZ8aeyFrDUyNsX2uCHnyFg2bgttFn_ldb-NyRNllvv1KLvksWYHY'
     });
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers }).subscribe( data => {
-      console.log(data)
-    });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
   }
 }
